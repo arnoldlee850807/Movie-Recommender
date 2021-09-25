88aece33f53d21212c86d47ebcb6ed89139e4d63
Arnold Lee <arnold.kn.lee@gmail.com>
2021-09-25 15:43:27 -0400

    Add badge, sizing, formatting

---
+++ background.js
@@ -0,0 +1,7 @@
+//  The action.onClicked event will not be dispatched if the extension action has specified a popup to show on click on the current tab.
+
+// chrome.action.onClicked.addListener(buttonClicked);
+
+// function buttonClicked() {
+//     console.log("button clicked");
+// }
