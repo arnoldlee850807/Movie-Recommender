4b636d45bb4ffb0ed65b99b1b0771e2e7a51b2a6
Arnold Lee <arnold.kn.lee@gmail.com>
2021-09-25 18:44:36 -0400

    connect popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
-console.log("popup running");
+document.addEventListener('DOMContentLoaded', function() {
+    var checkPageButton = document.getElementById('searchMovies');
+    checkPageButton.addEventListener('click', function() {
+        chrome.tabs.query({
+            active: true,
+            lastFocusedWindow: true
+        }, function(tabs) {
+            alert("hey");
+        });
+    }, false);
+}, false);
