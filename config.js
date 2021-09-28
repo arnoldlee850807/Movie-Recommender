49eb66ea34b843cf262653706f9fe833d321efdf
Adam Yen <adamyen@Adams-MacBook-Pro.local>
2021-09-28 15:57:55 -0400

    [feat]: fetch similar movies as soon as user searches a movie

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
-const APIKEY = '9b529d47e1b5df44dd507f6598535fbb'
-const BASE_URL = 'https://api.themoviedb.org/3/'
+const APIKEY = '9b529d47e1b5df44dd507f6598535fbb';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const IMG_URL_MEDIUM = 'https://image.tmdb.org/t/p/w185';
+const IMG_URL_SMALL = 'https://image.tmdb.org/t/p/w92';
 
-export { APIKEY, BASE_URL }
+export { APIKEY, BASE_URL, IMG_URL_MEDIUM, IMG_URL_SMALL }
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { IMG_URL_MEDIUM, IMG_URL_SMALL } from './config.js';
 import { searchMovie, getSimilarMovies } from './search.js';
 
 const searchBtn = document.getElementById('searchBtn');
@@ -7,59 +8,87 @@ searchBtn.addEventListener('click', async () => {
     const movie = textInput.split(' ').join('+');
     const res = await searchMovie(movie);
     Array.isArray(res.results) && res.results.length !== 0
-        ? appendData(res.results)
+        ? displaySearchedMovie(res.results[0])
         : handleEmptyResults(textInput);
 });
 
 const handleEmptyResults = textInput => {
-    clearBase();
-    const base = document.getElementById("base");
-    let noRes = document.createElement("h4");
+    clearDiv(3);
+    const searched = document.getElementById('searched');
+    let noRes = document.createElement('h4');
     noRes.appendChild(document.createTextNode(`No results for ${textInput}`))
-    base.appendChild(noRes);
+    searched.appendChild(noRes);
 }
 
-const appendData = results => {
-    clearBase();
-    const divs = results.map(res => {
+const displaySearchedMovie = async movie => {
+    clearDiv(1);
+    const searched = document.getElementById('searched');
+    let div = document.createElement('div');
+    const movieId = movie.id && movie.id;
+    const title = movie.original_title && movie.original_title;
+    const releaseDate = movie.release_date && movie.release_date;
+    const overView = movie.overview && movie.overview;
+    const posterPath = movie.poster_path && movie.poster_path;
+    let titleElm = document.createElement('h4');
+    titleElm.appendChild(document.createTextNode(title));
+    let releaseDateElm = document.createElement('p');
+    releaseDateElm.appendChild(document.createTextNode(`Release date: ${releaseDate}`));
+    let overViewElm = document.createElement('p');
+    overViewElm.appendChild(document.createTextNode(overView));
+    let posterElm = document.createElement('img');
+    posterElm.src = `${IMG_URL_MEDIUM}${posterPath}`;
+    div.appendChild(posterElm);
+    div.appendChild(titleElm);
+    div.appendChild(releaseDateElm);
+    div.appendChild(overViewElm);
+    searched.appendChild(div);
+    const similarMovies = await getSimilarMovies(movieId);
+    appendReccomendations(title, similarMovies.results.slice(1));
+}
+
+const appendReccomendations = (searchedMovie, movies) => {
+    clearDiv(2);
+    const divs = movies.map(m => {
         let div = document.createElement('div');
-        const movieId = res.id && res.id;
-        const title = res.original_title && res.original_title;
-        const releaseDate = res.release_date && res.release_date;
-        const overView = res.overview && res.overview;
-        const posterPath = res.poster_path && res.poster_path;
-        let titleElm = document.createElement("h4");
+        const title = m.original_title && m.original_title;
+        const posterPath = m.poster_path && m.poster_path;
+        let titleElm = document.createElement('h4');
         titleElm.appendChild(document.createTextNode(title));
-        let releaseDateElm = document.createElement("p");
-        releaseDateElm.appendChild(document.createTextNode(releaseDate));
-        let overViewElm = document.createElement("p");
-        overViewElm.appendChild(document.createTextNode(overView));
-        let posterElm = document.createElement("img")
-        posterElm.src = `https://image.tmdb.org/t/p/w185${posterPath}`;
-        let btn = document.createElement("button");
-        btn.innerText = 'Search similar movies';
-        btn.onclick = () => appendSimilarMovies(movieId);
-        div.appendChild(posterElm)
-        div.appendChild(titleElm)
-        div.appendChild(releaseDateElm)
-        div.appendChild(overViewElm)
-        div.appendChild(btn)
-        return div
+        let posterElm = document.createElement('img');
+        posterElm.src = `${IMG_URL_SMALL}${posterPath}`;
+        let btn = document.createElement('button');
+        btn.innerText = 'Get recommendation';
+        btn.onclick = () => getNewRecommedations(m);
+        div.appendChild(posterElm);
+        div.appendChild(titleElm);
+        div.appendChild(btn);
+        return div;
     });
-    let base = document.getElementById('base');
+    let content = document.getElementById('content');
+    const header = document.createElement('h3');
+    header.appendChild(document.createTextNode(`Movie recommendation based on ${searchedMovie}`));
+    content.appendChild(header);
     divs.forEach(div => {
-        base.appendChild(div)
+        content.appendChild(div);
     });
 }
 
-const appendSimilarMovies = async movieId => {
-    clearBase();
-    const movies = await getSimilarMovies(movieId);
-    console.log(movies)
-    movies.results && appendData(movies.results)
+const getNewRecommedations = async movie => {
+    clearDiv(3);
+    displaySearchedMovie(movie);
 }
 
-const clearBase = () => {
-    const base = document.getElementById("base");
-    base.innerHTML = "";
+const clearDiv = (x = 2) => {
+    switch(x) {
+        case 1:
+            document.getElementById('searched').innerHTML = '';
+            break;
+        case 2:
+            document.getElementById('content').innerHTML = '';
+            break;
+        case 3:
+            document.getElementById('searched').innerHTML = '';
+            document.getElementById('content').innerHTML = '';
+            break;
+    }
 }
